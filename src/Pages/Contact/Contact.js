0ecd5df570c4f5fa26d5238cd4da8eb052852747
Saiyadul77 <saiyadul77@gmail.com>
2022-09-19 14:05:59 +0600

    added contact and footer

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PrimaryButton from '../Shared/PrimaryButton';
+import appointment from '../../assets/images/appointment.png'
+
+const Contact = () => {
+    return (
+        <section
+        style={{
+            background:`url(${appointment})`
+        }}
+        className="py-10 my-32"
+        >
+            <div className='text-center '>
+                <div className=''>
+                    <h4 className='text-primary text-2xl pb-3'>Contact Us</h4>
+                    <h3 className='text-4xl pb-10 text-white '>Stay connected with us</h3>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <div class="form-control w-2/4 ">
+                        <label class="input-group input-group-vertical mb-2">
+                            <input type="text" placeholder="Email Address" class="input input-bordered" />
+                        </label>
+                        <label class="input-group input-group-vertical mb-2">
+                            <input type="text" placeholder="Subject" class="input input-bordered" />
+                        </label>
+                        <label class="input-group input-group-vertical mb-2">
+                            <textarea class="textarea textarea-bordered" placeholder="Bio"></textarea>
+                        </label>
+                        <div className='pt-5'>
+                            <PrimaryButton>Submit</PrimaryButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Contact from '../Contact/Contact';
 import Exceptional from '../Exceptional/Exceptional';
 import Info from '../Info/Info';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
+import Footer from '../Shared/Footer/Footer';
 import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
@@ -15,6 +17,8 @@ const Home = () => {
             <Exceptional></Exceptional>
             <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
